764ae142bb71ff52ba1b1e3add13311b98bbeee5
n3rada <n3rada@pm.me>
2021-04-09 20:18:44 +0200

    Lot of changes

--- src/parts/code/example2.py
+++ src/parts/code/example2.py
@@ -1,17 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def zfill_b(byteA, n: int):
+import ressources.tools as t
+import socket
+import struct
+
+class TCP:
     """
-    Fill byte till length n.
-
-    Output: bytes
+    Represent a TCP packet.
+    Nether case nor order are important.
     """
+    
+    def __init__(self, src_host:str, dst_host:str, src_port:str, dst_port:int, seq_num:int = 0, ack_num:int = 0, win_size:int = 8192, flags:str = "001011010", length:int = 612):
+        """
+        Constructor of TCP packet. 
+        """
+        self.src_host = src_host
+        self.dst_host = dst_host
+        self.src_port = src_port
+        self.dst_port = dst_port
+        self.seq_num = seq_num
+        self.ack_num = ack_num
+        self.win_size = win_size   
+        self.flags = int(flags,2) # car 0b001011010 == 90
+        self.length = length
 
-    if not isinstance(byteA, bytearray):
-        byteA = bytearray(byteA)
+    def build(self) -> bytes:
+        packet = struct.pack(
+            '!HHIIBBHHH',
+            self.src_port,  # Source Port
+            self.dst_port,  # Destination Port
+            self.seq_num,              # Sequence Number
+            self.ack_num,              # Acknoledgement Number
+            5 << 4,         # Data Offset
+            self.flags,     # Flags
+            self.win_size,           # Window
+            0,              # Checksum (initial value)
+            0               # Urgent pointer
+        )
 
-    while n > len(byteA):
-        byteA.insert(0, 0)
+        pseudo_hdr = struct.pack(
+            '!4s4sHH',
+            socket.inet_aton(self.src_host),    # Source Address
+            socket.inet_aton(self.dst_host),    # Destination Address
+            socket.IPPROTO_TCP,                 # PTCL = 6
+            len(packet) + self.length           # TCP Length
+        )
 
-    return bytes(byteA)
+        packet += t.payload(self.length) # adding payload
+
+        checksum = t.checksum(pseudo_hdr + packet)
+
+        # calculate the checksum and insert it back into the packet
+
+        packet = packet[:16] + struct.pack('H', checksum) + packet[18:]
+
+        return packet
