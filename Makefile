7d950c3738ddac26561b97d0b7f9dc19381bbd00
n3rada <n3rada@pm.me>
2021-04-26 19:52:10 +0200

    Huge update

--- Makefile
+++ Makefile
@@ -2,18 +2,18 @@
 
 # Generate PDF
 default:
-	latexmk -pdf main.tex
+	latexmk -pdf rapportUTT.tex
 
 # Generate PNG from pdf
 preview: default
 	convert -density 150 main.pdf -quality 90 main.png
 
-# Prepare archive to be used on overleaf
+# On prépare l'archive utilisable sur Overleaf (par exemple)
 archive:
 	rm -rf build
 	mkdir build
 
-	cp rUTT.cls main.tex LICENSE README.md .gitignore .latexmkrc Makefile build/
+	cp rUTT.cls rapportUTT.tex LICENSE README.md .gitignore .latexmkrc Makefile build/
 	cp -r src Ressources-graphiques build/
 
 	cd build &&	zip -r ../latex-rapport-UTT.zip *
@@ -23,9 +23,12 @@ prepare_deploy: preview archive
 	rm -rf deploy
 	mkdir deploy
 
-	cp main.pdf latex-rapport-UTT.zip rapport-0.png rapport-1.png deploy/
+	cp rapportUTT.pdf latex-rapport-UTT.zip deploy/
+	cp -r examplesPNG deploy/
 
 # Remove all temporary files
 clean:
 	latexmk -C
 	rm -rf build deploy *.png *.zip
+
+#rm -rf *.brf *.fls *.out *.log *.synctex.gz *.toc *.fdb_latexmk *.aux
--- src/parts/code/example2.py
+++ src/parts/code/example2.py
@@ -1,58 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import ressources.utils as ut
 
-import ressources.tools as t
-import socket
-import struct
 
-class TCP:
+def inv(a: int, m: int, Verbose: bool = False):
     """
-    Represent a TCP packet.
-    Nether case nor order are important.
+    Returns inverse of a mod m.
+
+    If a and m are prime to each other, then there is an a^(-1) such that a^(-1) * a is congruent to 1 mod m.
     """
-    
-    def __init__(self, src_host:str, dst_host:str, src_port:str, dst_port:int, seq_num:int = 0, ack_num:int = 0, win_size:int = 8192, flags:str = "001011010", length:int = 612):
-        """
-        Constructor of TCP packet. 
-        """
-        self.src_host = src_host
-        self.dst_host = dst_host
-        self.src_port = src_port
-        self.dst_port = dst_port
-        self.seq_num = seq_num
-        self.ack_num = ack_num
-        self.win_size = win_size   
-        self.flags = int(flags,2) # car 0b001011010 == 90
-        self.length = length
 
-    def build(self) -> bytes:
-        packet = struct.pack(
-            '!HHIIBBHHH',
-            self.src_port,  # Source Port
-            self.dst_port,  # Destination Port
-            self.seq_num,              # Sequence Number
-            self.ack_num,              # Acknoledgement Number
-            5 << 4,         # Data Offset
-            self.flags,     # Flags
-            self.win_size,           # Window
-            0,              # Checksum (initial value)
-            0               # Urgent pointer
-        )
+    # Error raising
 
-        pseudo_hdr = struct.pack(
-            '!4s4sHH',
-            socket.inet_aton(self.src_host),    # Source Address
-            socket.inet_aton(self.dst_host),    # Destination Address
-            socket.IPPROTO_TCP,                 # PTCL = 6
-            len(packet) + self.length           # TCP Length
-        )
+    if ut.euclid(a, m) != 1:
+        if Verbose:
+            print(f"gcd({a}, {m}) = {ut.euclid(a, m)} != 1 thus you cannot get an invert of {a}.")
+        raise ValueError(f"gcd({a}, {m}) != 1 thus you cannot get an invert of {a}.")
+        # a modular multiplicative inverse can be found directly
 
-        packet += t.payload(self.length) # adding payload
+    if a == 0:
+        if Verbose:
+            print("a = 0 and 0 cannot have multiplicative inverse ( 0 * nothing = 1 ) .")
+        raise ValueError("0 cannot have multiplicative inverse.")
 
-        checksum = t.checksum(pseudo_hdr + packet)
+    # Next
 
-        # calculate the checksum and insert it back into the packet
+    if ut.millerRabin(m) and m % a != 0:
+        # A simple consequence of Fermat's little theorem is that if p is prime and does not divide a
+        # then a^−1 ≡ a^(p − 2) (mod p) is the multiplicative
+        if Verbose:
+            print(f"From Fermat's little theorem, because {m} is prime and does not divide {a} so: {a}^-1 = {a}^({m}-2) mod {m}")
+        u = ut.square_and_multiply(a, m - 2, m)
 
-        packet = packet[:16] + struct.pack('H', checksum) + packet[18:]
+    elif ut.coprime(a, m) and m < (1 << 20):
+        # From Euler's theorem, if a and n are coprime, then a^−1  congruent to a^(phi(n) − 1) (mod n).
+        if Verbose:
+            print(f"From Euler's theorem, because {a} and {m} are coprime -> {a}^-1 = {a}^(phi({m})-1) mod {m}")
 
-        return packet
+        u = ut.square_and_multiply(a, phi(m, 1, 1, Verbose) - 1, m)
+
+    else:
+        if Verbose:
+            print("Modular inverse u solves the given equation: a.u+m.v=1.\n Let's use the euclid extended algorithm tho.")
+
+        # Modular inverse u solves the given equation: a.u+m.v=1
+        # n number of iterations
+        _, u, _, _, _ = ut.euclid_ext(a, m, Verbose)
+
+        if u < 0:
+            u += m
+
+    if Verbose:
+        return u, f"u = {u} + {m}k, k in Z"
+
+    return u
